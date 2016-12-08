746c1c8b586607807605a77a8100a2fccb60ff41
Benedikt Maier <bmaier@submit.mit.edu>
2016-12-08 14:14:12 -0500

    Cleanup

--- utilities/check_number_of_weights.py
+++ utilities/check_number_of_weights.py
@@ -5,15 +5,11 @@ from ROOT import TH1F, TFile, TTree, TString, gSystem, gROOT, AddressOf, TLorent
 import sys 
 import os
 import numpy as np
-#cmsswbase = TString.getenv("CMSSW_BASE")
 
-print 'Loading FW Lite setup.\n'
 gSystem.Load("libFWCoreFWLite.so") 
 ROOT.AutoLibraryLoader.enable()
 gSystem.Load("libDataFormatsFWLite.so")
 gSystem.Load("libDataFormatsPatCandidates.so")
-
-#from ROOT import TreeStruct
 
 events = Events (sys.argv[1])
 weighthandle = Handle ('LHEEventProduct')
@@ -22,22 +18,17 @@ weightlabel = ("source")
 evt_i = 0
 stat_i = 0
 
-#Loop through all events
 number_of_weights = 0
 
 for event in events:
         if evt_i > 0:
                 break
 
-        #Fill LHE event weights
         event.getByLabel(weightlabel,weighthandle)      
         Weightvector = weighthandle.product()
-	#print Weightvector.weights().size()
 	number_of_weights = Weightvector.weights().size()
 	
 	evt_i = evt_i + 1
-
-
 
 if number_of_weights != int(sys.argv[2]):
 	print number_of_weights
@@ -45,5 +36,3 @@ if number_of_weights != int(sys.argv[2]):
 	os.system('rm -f %s' % sys.argv[1])
 else:
 	os.system('mv %s %s' % (sys.argv[1], sys.argv[1].replace('.root','_X.root')))
-
-
